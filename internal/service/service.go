542df616e71475e8ceb548a010493e7e4484ff3c
Alex <alex@example.com>
2026-05-21 15:51:41 +0300

    Added service subscriptions

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,21 +2,19 @@ package service
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/Alex-Blacks/subscriptions/internal/domain"
-	"github.com/Alex-Blacks/subscriptions/internal/storage"
 )
 
 type Service struct {
-	storage storage.Storage
-	sub     domain.SubscriptionRepository
+	sub domain.SubscriptionRepository
 }
 
-func NewService(st storage.Storage, sub domain.SubscriptionRepository) *Service {
+func NewService(sub domain.SubscriptionRepository) *Service {
 	return &Service{
-		storage: st,
-		sub:     sub,
+		sub: sub,
 	}
 }
 
@@ -29,7 +27,7 @@ func (s *Service) WithTx(ctx context.Context, fn func(q domain.Querier) error) (
 	defer func() {
 		if err != nil {
 			if rollbackErr := tx.Rollback(ctx); rollbackErr != nil {
-				err = fmt.Errorf("tx err: %w, rollback err: %w", err, rollbackErr)
+				err = errors.Join(err, rollbackErr)
 			}
 			return
 		}
@@ -38,22 +36,43 @@ func (s *Service) WithTx(ctx context.Context, fn func(q domain.Querier) error) (
 		}
 	}()
 
-	return fn(tx)
+	err = fn(tx)
+	return err
 }
 
 func (s *Service) CreateSubscription(ctx context.Context, input domain.CreateSubscriptionInput) (int, error) {
 	var id int
 	if err := s.WithTx(ctx, func(q domain.Querier) error {
-		var err error
-		id, err = s.sub.CreateSubscription(ctx, q, input)
-		return err
+		var createErr error
+		id, createErr = s.sub.CreateSubscription(ctx, q, input)
+		return createErr
 	}); err != nil {
 		return 0, err
 	}
 	return id, nil
 }
-func (s *Service) GetSubscriptionByID(ctx context.Context, id int) (domain.Subscription, error)
-func (s *Service) DeleteSubscription(ctx context.Context, id int) error
-func (s *Service) UpdateSubscription(ctx context.Context, id int, update domain.UpdateSubscriptionInput) (domain.Subscription, error)
-func (s *Service) ListSubscription(ctx context.Context, filter domain.ListFilter) ([]domain.Subscription, error)
-func (s *Service) SumSubscriptionPrice(ctx context.Context, filter domain.ListFilter) (int, error)
+func (s *Service) GetSubscriptionByID(ctx context.Context, id int) (domain.Subscription, error) {
+	return s.sub.GetSubscriptionByID(ctx, s.storage, id)
+}
+func (s *Service) DeleteSubscription(ctx context.Context, id int) error {
+	return s.WithTx(ctx, func(q domain.Querier) error {
+		return s.sub.DeleteSubscription(ctx, q, id)
+	})
+}
+func (s *Service) UpdateSubscription(ctx context.Context, id int, update domain.UpdateSubscriptionInput) (domain.Subscription, error) {
+	var sub domain.Subscription
+	if err := s.WithTx(ctx, func(q domain.Querier) error {
+		var updateErr error
+		sub, updateErr = s.sub.UpdateSubscription(ctx, q, id, update)
+		return updateErr
+	}); err != nil {
+		return domain.Subscription{}, err
+	}
+	return sub, nil
+}
+func (s *Service) ListSubscription(ctx context.Context, filter domain.ListFilter) ([]domain.Subscription, error) {
+	return s.sub.ListSubscription(ctx, s.storage, filter)
+}
+func (s *Service) SumSubscriptionPrice(ctx context.Context, filter domain.ListFilter) (int, error) {
+	return s.sub.SumSubscriptionPrice(ctx, s.storage, filter)
+}
